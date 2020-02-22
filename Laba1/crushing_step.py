ae939e393d54c9c85cdcdbebbad703e279b6360b
bateiko <bateiko0713@gmail.com>
2020-02-22 20:57:51 +0200

    all done

--- Laba1/crushing_step.py
+++ Laba1/crushing_step.py
@@ -11,24 +11,25 @@ count = 1
 xk = np.array([0, 0], dtype=float)
 vector_h1 = f.vector_h(xk)
 alpha_k1 = const_betta
-# alpha_k1 = np.float(0.5)
 const_func_xk_1 = f.func(xk + f.multiply_on_const(vector_h1, alpha_k1))
 
-print(f"\ncount = \t{count}\n xk =\t{xk}\n h =\t{vector_h1}\n "
-      f"alpha =\t {alpha_k1}\n const_func = \t{const_func_xk_1}")
+file_name = "crushing_step.txt"
 
-while np.abs(const_func_xk_1 - f.func(xk)) > f.h:
+with open(file_name, "w+") as file:
+    line = f"\ncount = \t{count}\n xk =\t{xk}\n h =\t{vector_h1}\n alpha =\t {alpha_k1}"
+    file.write(line)
 
-    xk = xk + np.array(f.multiply_on_const(vector_h1, alph=alpha_k1))
-    const_func_xk_1 = f.func(xk + f.multiply_on_const(vector_h1, alpha_k1))
+    while np.abs(const_func_xk_1 - f.func(xk)) > f.h:
 
-    vector_h1 = f.vector_h(xk)
-    alpha_k1 = np.dot(alpha_k1, const_lambda)
+        xk = xk + np.array(f.multiply_on_const(vector_h1, alph=alpha_k1))
+        const_func_xk_1 = f.func(xk + f.multiply_on_const(vector_h1, alpha_k1))
 
-    count += 1
-    if count < 5:
-        print(f"\ncount = \t{count}\n xk =\t{xk}\nfunc(xk) = {f.func(xk)}\n "
-              f"h =\t{vector_h1}\n alpha =\t {alpha_k1}\n const_func = \t{const_func_xk_1} ")
+        vector_h1 = f.vector_h(xk)
+        alpha_k1 = np.dot(alpha_k1, const_lambda)
+        count += 1
+
+        line = f"\ncount = \t{count}\n xk =\t{xk}\nfunc(xk) = {f.func(xk)}\n h =\t{vector_h1} \nalpha =\t {alpha_k1}"
+        file.write(line)
 
 print(f"****count = \t{count}\nx* =\t{xk} \n func(xk) = {f.func(xk)}\n gradient(xk) = \t{f.gradient(xk)}")
 print(f"func1:\t{f.func(xk)}\nfunc2:\t{f.func([2.1, 1])}")
--- Laba1/with_const_alpha.py
+++ Laba1/with_const_alpha.py
@@ -3,17 +3,13 @@ from numpy import linalg as npl
 
 import functions as f
 
-alpha_k = lambda x: np.float(
-    np.dot(f.vector_h(x), f.vector_h(x)) / np.dot(np.dot(f.matrix_A, f.vector_h(x)), f.vector_h(x)))
 
 count = 1
 xk = np.array([0, 0], dtype=float)
 vector_h1 = f.vector_h(xk)
-alpha_k1 = alpha_k(xk)
+alpha_k1 = np.float(0.01)
 
-const_func_xk_1 = np.float(f.func(xk + np.array(f.multiply_on_const(vector_h1, alph=alpha_k1))))
-
-file_name = 'descent_method.txt'
+file_name = 'with_const_alpha.txt'
 
 with open(file_name, "w+") as file:
     line = f"\ncount = \t{count}\n xk =\t{xk}\n h =\t{vector_h1}\n alpha =\t {alpha_k1}"
@@ -22,9 +18,6 @@ with open(file_name, "w+") as file:
     while npl.norm(vector_h1) > f.h:
         xk = xk + np.array(f.multiply_on_const(vector_h1, alph=alpha_k1))
         vector_h1 = f.vector_h(xk)
-        alpha_k1 = alpha_k(xk)
-
-        const_func_xk_1 = np.float(f.func(xk + np.array(f.multiply_on_const(vector_h1, alph=alpha_k1))))
         count += 1
 
         line = f"\ncount = \t{count}\n xk =\t{xk}\nfunc(xk) = {f.func(xk)}\n h =\t{vector_h1}\nalpha =\t {alpha_k1}"
